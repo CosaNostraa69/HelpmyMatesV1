024ad71c32c4d5b0d1dae7382e50dbecbc2b882b
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-01-01 10:59:15 +0100

    Update styles and title in index.html and remove unused code in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,134 +3,14 @@ import './App.css';
 import 'primereact/resources/themes/lara-light-cyan/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import { Button } from "primereact/button";
-import { InputText } from 'primereact/inputtext';
-import { MegaMenu } from 'primereact/megamenu';
-                
-const items = [
-  {
-      label: 'Videos', icon: 'pi pi-fw pi-video',
-      items: [
-          [
-              {
-                  label: 'Video 1',
-                  items: [{ label: 'Video 1.1' }, { label: 'Video 1.2' }]
-              },
-              {
-                  label: 'Video 2',
-                  items: [{ label: 'Video 2.1' }, { label: 'Video 2.2' }]
-              }
-          ],
-          [
-              {
-                  label: 'Video 3',
-                  items: [{ label: 'Video 3.1' }, { label: 'Video 3.2' }]
-              },
-              {
-                  label: 'Video 4',
-                  items: [{ label: 'Video 4.1' }, { label: 'Video 4.2' }]
-              }
-          ]
-      ]
-  },
-  {
-      label: 'Users', icon: 'pi pi-fw pi-users',
-      items: [
-          [
-              {
-                  label: 'User 1',
-                  items: [{ label: 'User 1.1' }, { label: 'User 1.2' }]
-              },
-              {
-                  label: 'User 2',
-                  items: [{ label: 'User 2.1' }, { label: 'User 2.2' }]
-              },
-          ],
-          [
-              {
-                  label: 'User 3',
-                  items: [{ label: 'User 3.1' }, { label: 'User 3.2' }]
-              },
-              {
-                  label: 'User 4',
-                  items: [{ label: 'User 4.1' }, { label: 'User 4.2' }]
-              }
-          ],
-          [
-              {
-                  label: 'User 5',
-                  items: [{ label: 'User 5.1' }, { label: 'User 5.2' }]
-              },
-              {
-                  label: 'User 6',
-                  items: [{ label: 'User 6.1' }, { label: 'User 6.2' }]
-              }
-          ]
-      ]
-  },
-  {
-      label: 'Events', icon: 'pi pi-fw pi-calendar',
-      items: [
-          [
-              {
-                  label: 'Event 1',
-                  items: [{ label: 'Event 1.1' }, { label: 'Event 1.2' }]
-              },
-              {
-                  label: 'Event 2',
-                  items: [{ label: 'Event 2.1' }, { label: 'Event 2.2' }]
-              }
-          ],
-          [
-              {
-                  label: 'Event 3',
-                  items: [{ label: 'Event 3.1' }, { label: 'Event 3.2' }]
-              },
-              {
-                  label: 'Event 4',
-                  items: [{ label: 'Event 4.1' }, { label: 'Event 4.2' }]
-              }
-          ]
-      ]
-  },
-  {
-      label: 'Settings', icon: 'pi pi-fw pi-cog',
-      items: [
-          [
-              {
-                  label: 'Setting 1',
-                  items: [{ label: 'Setting 1.1' }, { label: 'Setting 1.2' }]
-              },
-              {
-                  label: 'Setting 2',
-                  items: [{ label: 'Setting 2.1' }, { label: 'Setting 2.2' }]
-              },
-              {
-                  label: 'Setting 3',
-                  items: [{ label: 'Setting 3.1' }, { label: 'Setting 3.2' }]
-              }
-          ],
-          [
-              {
-                  label: 'Technology 4',
-                  items: [{ label: 'Setting 4.1' }, { label: 'Setting 4.2' }]
-              }
-          ]
-      ]
-  }
-];
 
-const start = <img alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" height="40" className="mr-2"></img>;
-const end = <InputText placeholder="Search" type="text" />;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <MegaMenu model={items} orientation="horizontal" start={start} end={end} breakpoint="960px" />
          <img src={logo} className="App-logo" alt="logo" />
           
-        <Button label="Click Me" icon="pi pi-check" />
        
       </header>
     </div>
