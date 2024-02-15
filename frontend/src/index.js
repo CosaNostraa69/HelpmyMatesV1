1ecdbc9fd1e5068ffae0f3e1f755431fea750fee
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-02-15 10:43:24 +0100

    Update Auth0Provider props in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,12 +15,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        redirectUri={window.location.origin}>
-        <App />
-      </Auth0Provider>
+    <Auth0Provider
+  domain={domain}
+  clientId={clientId}
+  authorizationParams={{ redirect_uri: window.location.origin }}>
+  <App />
+</Auth0Provider>
+
     </BrowserRouter>
   </React.StrictMode>
 );
